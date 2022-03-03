c2505228950a55bf4f6caa58e97c9926514b7ced
ZeR0 <99215787+ZeR0Jeayer@users.noreply.github.com>
2022-03-03 19:22:17 -0300

    Add files via upload

---
+++ pingnet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$1" == "" ]
+then
+echo "Exemplo de uso: $0 10.0.0"
+else
+for teste in {1..254};do
+text="Conexão feita!"
+ping -c1 $1.$teste | grep "64 bytes" | cut -d " " -f 4 | sed 's/.$//'
+done
+fi
